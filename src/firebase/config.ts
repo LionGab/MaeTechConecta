c2836d3bbf869fcf77f3a0c580e865c285cc3888
Gabriel Vesz <eugabrielmktd@gmail.com>
2025-10-23 23:39:13 +0000

    Configurações do projeto Geral Cloud Messaging Integrações Contas de ser

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,12 @@
-// Este arquivo contém a configuração do seu projeto Firebase.
-// É crucial que você substitua os valores de placeholder abaixo pelos valores reais do seu projeto.
-// Você pode encontrar esses valores no console do Firebase, nas configurações do seu projeto web.
+
+// Este arquivo contém a configuração do Firebase para o aplicativo.
+// Os valores abaixo foram atualizados com as credenciais do seu projeto.
 
 export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJECT_ID.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJECT_ID.appspot.com",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID",
-  measurementId: "SEU_MEASUREMENT_ID" // Opcional
+  apiKey: "AIzaSyCBp3XJkLAVUbJ3zPbgKG_WJdeTHK0LaOY",
+  authDomain: "studio-1781279445-a66f7.firebaseapp.com",
+  projectId: "studio-1781279445-a66f7",
+  storageBucket: "studio-1781279445-a66f7.appspot.com",
+  messagingSenderId: "739802775325",
+  appId: "1:739802775325:web:47a05d59d22721e6e2abf4"
 };
