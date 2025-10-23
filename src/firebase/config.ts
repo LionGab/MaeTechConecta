b2ded009ed1b8b9ffcd782a1cf64ab67cd8f02b9
Gabriel Vesz <eugabrielmktd@gmail.com>
2025-10-23 21:55:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1781279445-a66f7",
+  "appId": "1:739802775325:web:47a05d59d22721e6e2abf4",
+  "apiKey": "AIzaSyCBp3XJkLAVUbJ3zPbgKG_WJdeTHK0LaOY",
+  "authDomain": "studio-1781279445-a66f7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "739802775325"
+};
