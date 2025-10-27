14f53f6d97f27b41a0ef6c1da074741b0691de4e
Gabriel Vesz <eugabrielmktd@gmail.com>
2025-10-27 07:20:59 +0000

    adicione imagens do repositorio

--- next.config.ts
+++ next.config.ts
@@ -15,24 +15,6 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https' ,
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
